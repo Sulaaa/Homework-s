3ed1afe5e79a091dafd02ab316c556e61245e42a
Sultanbek Jenisbekov <70880353+Sulaaa@users.noreply.github.com>
2020-09-09 13:33:29 +0500

    Add files via upload

---
+++ Sula s Homework.py
@@ -0,0 +1,18 @@
+# Drive-Y-Link Sula
+myName = "Sultanbek"
+
+mySurname = "Jenisbekov"
+
+age = "17"
+
+placeofbirth = "Atyraucity"
+
+groupe = "AutomatizationandControl"
+"/myName,mySurname,age, placeofbirth ,groupe"
+print(myName, mySurname, age, placeofbirth, groupe)
+print("имя: ", myName)
+print("Фамилия :", mySurname)
+print("Возраст: ", age)
+print("Место рождения:", placeofbirth)
+print("Группа:", groupe)
+
